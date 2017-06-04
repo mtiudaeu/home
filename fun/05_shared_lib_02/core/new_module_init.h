802847c5c8d6244bd3d598ad5469a4d50d8adac9
mathieu <m.drapeau9@gmail.com>
2017-06-04 17:39:53 -0400

    update

--- fun/05_shared_lib_02/core/new_module_init.h
+++ fun/05_shared_lib_02/core/new_module_init.h
@@ -6,18 +6,16 @@
 
 status init_module(module& module, data_manager& data_manager)
 {
-  status status;
-  auto clean_up_func = [&] {
+  auto clean_up_module = [&module] {
     dlclose(module.lib_handle);
     module.lib_handle = 0x0;
     module.init_cb = 0x0;
     module.uninit_cd = 0x0;
     module.step_cd = 0x0;
     module.st_ino = 0;
-
-    status.error = true;
   };
 
+  status status;
   if(!module.lib_path) {
     LOG_DEBUG("!module.lib_path"):
     return status;
@@ -27,6 +25,18 @@ status init_module(module& module, data_manager& data_manager)
   if (stat(module.lib_path, &attr) != 0) {
     LOG_ERROR("%s : %s", strerror(errno), module.lib_path);
     return status;
+  }
+
+  if(module.st_ino == attr.st_ino) {
+    LOG_DEBUG("Don't need reloading");
+    return status;
+  }
+  if(module.st_ino != 0) {
+    // module is open. uninit before init
+    if(module.uninit_cb) {
+      status = module.uninit_cb();
+      clean_up_module();
+    }
   }
 
   module.lib_handle = dlopen(module.lib_path, RTLD_NOW);
@@ -41,7 +51,8 @@ status init_module(module& module, data_manager& data_manager)
       dlsym(module.lib_handle, "init"));
   if (!module.init_cb) {
     LOG_ERROR("dlsym : %s", dlerror());
-    clean_up_func();
+    clean_up_module();
+    status.error = true;
     return status;
   }
 
@@ -49,7 +60,8 @@ status init_module(module& module, data_manager& data_manager)
       dlsym(module.lib_handle, "uninit"));
   if (!module.uninit_cb) {
     LOG_ERROR("dlsym : %s", dlerror());
-    clean_up_func();
+    clean_up_module();
+    status.error = true;
     return status;
   }
 
@@ -57,14 +69,16 @@ status init_module(module& module, data_manager& data_manager)
       dlsym(module.lib_handle, "step"));
   if (!module.step_cb) {
     LOG_ERROR("dlsym : %s", dlerror());
-    clean_up_func();
+    clean_up_module();
+    status.error = true;
     return status;
   }
 
   status = module.init_cb(&data_manager);
   if(status.error) {
     LOG_ERROR("module.init_cb");
-    clean_up_func();
+    clean_up_module();
+    status.error = true;
     return status;
   }
 
--- fun/05_shared_lib_02/main.cpp
+++ fun/05_shared_lib_02/main.cpp
@@ -1,138 +1,37 @@
 #include "new_module_init.h"
 
-struct main_lib {
-  data_manager data_manager;
-};
-
-static std::unique_ptr<main_lib> init(module_status& module_status) {
-  LOG_DEBUG("init");
-  std::unique_ptr<main_lib> main_lib_ptr = std::make_unique<main_lib>();
-
-  module::library* global_data_library = 0x0;
-  module::library* catch_event_library = 0x0;
-  { //global_data
-    global_data_library = module::init(ROOT_PATH "global_data.so", module_status);
-    if (!global_data_library) {
-      LOG_ERROR("module::init");
-      module_status.error = true;
-      return 0x0;
-    }
-    if (module_status.error) {
-      LOG_ERROR("module::init");
-      return 0x0;
-    }
-    main_lib_ptr->libraries.push_back(global_data_library);
-  }
-  { //catch_event
-    catch_event_library = module::init(ROOT_PATH "catch_event.so", module_status);
-    if (!catch_event_library) {
-      LOG_ERROR("module::init");
-      module_status.error = true;
-      return 0x0;
-    }
-    if (module_status.error) {
-      LOG_ERROR("module::init");
-      return 0x0;
-    }
-    main_lib_ptr->libraries.push_back(catch_event_library);
-  }
-  { //update
-    module::library* libraries_dependencies[2] = {catch_event_library,
-                                              global_data_library};
-    module::library* library = module::init(ROOT_PATH "update.so", module_status,
-                                            libraries_dependencies, 2);
-    if (!library) {
-      LOG_ERROR("module::init");
-      module_status.error = true;
-      return 0x0;
-    }
-    if (module_status.error) {
-      LOG_ERROR("module::init");
-      return 0x0;
-    }
-    main_lib_ptr->libraries.push_back(library);
-  }
-  { //render
-    module::library* libraries_dependencies[1] = {global_data_library};
-    module::library* library = module::init(ROOT_PATH "render.so", module_status,
-                                            libraries_dependencies, 1);
-    if (!library) {
-      LOG_ERROR("module::init");
-      module_status.error = true;
-      return 0x0;
-    }
-    if (module_status.error) {
-      LOG_ERROR("module::init");
-      return 0x0;
-    }
-    main_lib_ptr->libraries.push_back(library);
-  }
-  { //context
-    module::library* library = module::init(ROOT_PATH "context.so", module_status);
-    if (!library) {
-      LOG_ERROR("module::init");
-      module_status.error = true;
-      return 0x0;
-    }
-    if (module_status.error) {
-      LOG_ERROR("module::init");
-      return 0x0;
-    }
-    main_lib_ptr->libraries.push_back(library);
-  }
-
-  return main_lib_ptr;
-}
-
-static status step(struct main_lib& main_lib) {
-  LOG_DEBUG("main_lib : step");
-
-  for (module::library* library : main_lib.libraries) {
-    assert(library);
-
-    // MDTMP revisit loading strategy
-    module_status step_status;
-    const module_status reload_status =
-        module::reload_if_needed(*library);
-    if (reload_status.error) {
-      LOG_ERROR("reload_if_needed");
-      step_status.error = true;
-      return step_status;
-    }
-
-    step_status = module::step(*library);
-    if (step_status.error) {
-      LOG_ERROR("step_status.error");
-      return step_status;
-    }
-    if (step_status.info_code == STEP_INFO_STOPPING) {
-      return step_status;
-    }
-  }
-  return module_status();
-}
-
 int main() {
   status status;
-  std::unique_ptr<main_lib> main_lib_ptr = init(status);
-  if (!main_lib_ptr) {
-    LOG_ERROR(!main_lib_ptr);
-    return 1;
-  }
-  if (status.error) {
-    LOG_ERROR("status.error : %zu", status.info_code);
-    return 1;
-  }
 
+  data_manager* data_manager = new data_manager();
+  
+  const size_t loop_reload_max = 10;
+  size_t loop_reload_count = 0;
   for (;;) {
-    status = step(*main_lib_ptr);
-    if (status.error) {
-      LOG_ERROR("status.error : %zu", status.info_code);
-      break;
+    // FIXME Doing all reloading at the same time could hit performance.
+    if(loop_reload_count => loop_reload_max) {
+      loop_reload_count = 0;
+      status = init_all_module(*data_manager);
+      if(status.error) {
+        LOG_ERROR("init_all_module");
+        return 1;
+      }
     }
-    if (status.info_code == STEP_INFO_STOPPING) {
-      LOG_DEBUG("status.info_code == STEP_INFO_STOPPING");
-      break;
+
+    for(auto module : data_manager->modules) {
+      assert(module);
+      if(!module->step_cb) {
+        continue;
+      }
+      status = module->step_cb();
+      if (status.error) {
+        LOG_ERROR("status.error");
+        return 1;
+      }
+      if (status.info_code == STEP_INFO_STOPPING) {
+        LOG_DEBUG("status.info_code == STEP_INFO_STOPPING");
+        return 0;
+      }
     }
   }
 }
