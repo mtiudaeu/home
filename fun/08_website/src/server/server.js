537212243d5056fc54f3bb7bf302751f978ec5be
Mathieu Drapeau <m.drapeau9@gmail.com>
2017-10-24 16:54:30 -0400

    update

--- fun/08_website/src/server/server.js
+++ fun/08_website/src/server/server.js
@@ -1,20 +1,21 @@
 "use strict";
 
 // Dependency
-let fs = require('fs');
-let https = require('https');
-//MDTMP let WebSocketServer = require('ws').Server;
+const fs = require('fs');
+const https = require('https');
+//MDTMP const WebSocketServer = require('ws').Server;
 
 // Https server
-let HTTPS_PORT = 8888;
+const HTTPS_PORT = 8888;
+const CONFIG_PUBLIC_PATH = 'public';
 let serverConfig = {
-key: fs.readFileSync('ssl_keys/key.pem'),
-     cert: fs.readFileSync('ssl_keys/cert.pem'),
+    key: fs.readFileSync('ssl_keys/key.pem'),
+    cert: fs.readFileSync('ssl_keys/cert.pem'),
 };
 let serverRequestHandle = function(request, response) {
-    let filepath = 'public'.concat(request.url);
-    if(filepath == 'public/') {
-        filepath = 'public/index.html';
+    let filepath = CONFIG_PUBLIC_PATH.concat(request.url);
+    if(filepath == (CONFIG_PUBLIC_PATH + '/')) {
+        filepath = CONFIG_PUBLIC_PATH + '/index.html';
     }
     fs.access(filepath, (err) => {
         if (err) {
@@ -26,7 +27,11 @@ let serverRequestHandle = function(request, response) {
             throw err;
         }
 
-        response.writeHead(200, {'Content-Type': 'text/html'});
+        if (filepath.endsWith('css')) {
+            response.writeHead(200, {'Content-Type': 'text/css'});
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+        }
         response.end(fs.readFileSync(filepath));
     });
 };
@@ -35,17 +40,17 @@ let httpsServer = https.createServer(serverConfig, serverRequestHandle);
 httpsServer.listen(HTTPS_PORT, '0.0.0.0');
 
 //MDTMP
-// WebSocket server
 /*
-   let wss = new WebSocketServer({server: httpsServer});
-   wss.on('connection', function(ws) {
-   ws.on('message', function(message) {
-   wss.broadcast(message);
-   });
-   });
-   wss.broadcast = function(data) {
-   for(let i in this.clients) {
-   this.clients[i].send(data);
-   }
-   };
- */
+// WebSocket server
+let wss = new WebSocketServer({server: httpsServer});
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+    wss.broadcast(message);
+    });
+});
+wss.broadcast = function(data) {
+    for(let i in this.clients) {
+        this.clients[i].send(data);
+    }
+};
+*/
