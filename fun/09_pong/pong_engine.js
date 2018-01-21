26a698958614398de7459c602ef99d82ff3856d4
Mathieu Drapeau <m.drapeau9@gmail.com>
2018-01-21 16:18:07 -0500

    update

--- fun/09_pong/pong_engine.js
+++ fun/09_pong/pong_engine.js
@@ -3,20 +3,44 @@
 class PongEngine {
 	constructor() {
 		
-		this.ball_pos_y = 0.5
-		this.ball_pos_x = 0.5
+		this.ball_pos_y = 0.0
+		this.ball_pos_x = 0.0
 		this.ball_diameter = 0.15
 		this.ball_speed = 0.1
-		this.ball_speed_direction = Math.PI * Math.random()
+		this.ball_speed_angle = 2 * Math.PI * Math.random()
+        this.ball_dy = Math.sin(this.ball_speed_angle)*this.ball_speed
+        this.ball_dx = Math.cos(this.ball_speed_angle)*this.ball_speed
 
-		this.paddle_pos_x = 0.5
+		this.paddle_pos_x = 0
 		this.paddle_pos_y = 0.05
 		this.paddle_width = 0.4
 		this.paddle_height = 0.05
 	}
 
-	update(time_delta) {
-// TODO add params : Move right, move left.
+	step() {
+		this.ball_pos_y += this.ball_dy
+		this.ball_pos_x += this.ball_dx
+
+		if(this.ball_pos_y > 1) {
+            this.ball_dy = -this.ball_dy
+        }
+		if(this.ball_pos_y < -1) {
+            this.ball_dy = -this.ball_dy
+        }
+		if(this.ball_pos_x > 1) {
+            this.ball_dx = -this.ball_dx
+        }
+		if(this.ball_pos_x < -1) {
+            this.ball_dx = -this.ball_dx
+        }
+
+        
+// TODO add inputs : Move right, move left.
+
+	// TODO increase speed as it goes
+	// TODO Calculate bounce angle
+	// TODO different angle for paddle hit?
+
 		//console.log("this.ball_pos_y", this.ball_pos_y)
 		//console.log("this.ball_pos_x", this.ball_pos_x)
 		//console.log("this.paddle_pos_x", this.paddle_pos_x)
--- fun/09_pong/ui.js
+++ fun/09_pong/ui.js
@@ -4,7 +4,7 @@
 //let HEIGHT = window.innerHeight
 const WIDTH = 400
 const HEIGHT = 400
-const CAMERA_DISTANCE = 1
+const CAMERA_DISTANCE = 2
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, WIDTH/HEIGHT, 0.1, 1000 );
@@ -35,12 +35,6 @@ let animate = function () {
 //TODO var d = new Date();
 //TODO var n = d.getTime();
 
-	// TODO movement should depend on time,
-	// TODO increase speed as it goes
-	// TODO gettime method?
-	// TODO generate random start angle, random method
-	// TODO Calculate bounce angle
-	// TODO different angle for paddle hit?
 /*
 	if(up) {
 		circle.position.y += 0.1;
@@ -49,7 +43,11 @@ let animate = function () {
 		circle.position.y -= 0.1;
 	}
 */
-	pong_engine.update()
+	pong_engine.step()
+
+	circle.position.y = pong_engine.ball_pos_y;
+	circle.position.x = pong_engine.ball_pos_x;
+
 	renderer.render(scene, camera);
 };
 
