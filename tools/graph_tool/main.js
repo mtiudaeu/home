0291b7236a375791c3f29cfc34cdb43252541fa2
Mathieu Drapeau <m.drapeau9@gmail.com>
2018-03-05 19:29:16 -0500

    update

--- tools/graph_tool/main.js
+++ tools/graph_tool/main.js
@@ -1,19 +1,17 @@
 "use strict"
 
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+ctx.font = "10px Arial";
+
+let drawEngine = new DrawEngine(ctx);
+
 window.onload = window.onresize = function() {
-    let c = document.getElementById("myCanvas");
     c.width = window.innerWidth;
     c.height = window.innerHeight;
-
-    let ctx = c.getContext("2d");
-    ctx.font = "10px Arial";
-
-    c.addEventListener('click', function(event) {
-        let drawEngine = new DrawEngine(ctx);
-        let x = event.pageX;
-        let y = event.pageY;
-        drawEngine.addItem(x,y);
-        drawEngine.drawItems();
-    }, false);
+    drawEngine.drawItems();
 }
 
+document.onkeydown = function(event) {
+    console.log(event.keyCode)
+};
