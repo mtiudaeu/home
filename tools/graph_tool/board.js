393980dfb7fbb0030d0222c20d64e07f79c9ca6d
Mathieu Drapeau <m.drapeau9@gmail.com>
2018-03-11 15:06:06 -0400

    update

--- tools/graph_tool/board.js
+++ tools/graph_tool/board.js
@@ -5,24 +5,39 @@ constructor(canva) {
     this.canva = canva
     this.context2d = canva.getContext("2d");
     this.context2d.font = "10px Arial";
-    this.items = []
+    this.cards = []
     this.currentText = ""
+}
+
+getCardWidth(card) {
+    let textDimensions = this.context2d.measureText(card.title);
+    //FIXME
+    const offset = 10;
+    return textDimensions.width + 2*offset; //FIXME
+}
+
+getCardHeight() {
+//FIXME
+    const offset = 10
+    return 10+2*offset; //FIXME
 }
 
 clearAndDrawAll() {
     this.context2d.clearRect(0, 0, this.canva.width, this.canva.height);
-    for (let i = 0; i < this.items.length; i++) {
-        this.drawItem(this.items[i]);
+    for (let i = 0; i < this.cards.length; i++) {
+        this.drawItem(this.cards[i]);
     }
 //FIXME
     this.drawString(this.currentText, 150, 150)
 }
 
-drawItem(item) {
+drawItem(card) {
+    let cardWidth = this.getCardWidth(card);
+    let cardHeight = this.getCardHeight();
     //FIXME
     const offset = 10
-    this.drawRectangle(item.x - offset, item.y, item.width, item.height);
-    this.drawString(item.title, item.x, item.y + 10 + offset);
+    this.drawRectangle(card.x - cardWidth/2, card.y - cardHeight/2, cardWidth, cardHeight);
+    this.drawString(card.title, card.x- cardWidth/2 + offset, card.y  - cardHeight/2 + 10 + offset);
 }
 
 drawRectangle(x,y,width,height) {
@@ -34,13 +49,12 @@ drawString(text, x, y) {
 }
 
 addItem(title, x, y) {
-    let item = new Card(
-        this.canva,
+    let card = new Card(
         title,
         x,
         y
     )
-    this.items.push(item)
+    this.cards.push(card)
 }
 
 keyPressed(keyCode) {
