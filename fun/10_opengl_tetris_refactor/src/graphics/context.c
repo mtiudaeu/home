b0c93b8ebd1b0798da99367a4ef60280d4b8e92c
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-02-26 23:14:05 -0400

    update

--- fun/10_opengl_tetris_refactor/src/graphics/context.c
+++ fun/10_opengl_tetris_refactor/src/graphics/context.c
@@ -54,43 +54,42 @@ size_t graphics_context_global_init()
     return 1;
   }
 
-  {  // OpenGL related
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-      LOG_ERROR("SDL_Init : %s", SDL_GetError());
-      return 1;
-    }
+  // OpenGL related
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    LOG_ERROR("SDL_Init : %s", SDL_GetError());
+    return 1;
+  }
 
-    global_graphics_context = calloc(1, sizeof(*global_graphics_context));
-    global_graphics_context->window = SDL_CreateWindow(
-        "Default Parameter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        1024, 768, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
-    if (!global_graphics_context->window) {
-      graphics_context_global_uninit();
-      LOG_ERROR("SDL_CreateWindow %s", SDL_GetError());
-      return 1;
-    }
+  global_graphics_context = calloc(1, sizeof(*global_graphics_context));
+  global_graphics_context->window = SDL_CreateWindow(
+      "Default Parameter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      1024, 768, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+  if (!global_graphics_context->window) {
+    graphics_context_global_uninit();
+    LOG_ERROR("SDL_CreateWindow %s", SDL_GetError());
+    return 1;
+  }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    global_graphics_context->gl_context =
-        SDL_GL_CreateContext(global_graphics_context->window);
-    if (global_graphics_context->gl_context == NULL) {
-      graphics_context_global_uninit();
-      LOG_ERROR("SDL_GL_CreateContext %s", SDL_GetError());
-      return 1;
-    }
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  global_graphics_context->gl_context =
+      SDL_GL_CreateContext(global_graphics_context->window);
+  if (global_graphics_context->gl_context == NULL) {
+    graphics_context_global_uninit();
+    LOG_ERROR("SDL_GL_CreateContext %s", SDL_GetError());
+    return 1;
+  }
 
-    // TODO validate if glewInit this need clean-up.
-    GLenum glew_status = glewInit();
-    if (glew_status != GLEW_OK) {
-      LOG_ERROR("glewInit %s", glewGetErrorString(glew_status));
-      graphics_context_global_uninit();
-      return 1;
-    }
-    if (!GLEW_VERSION_2_0) {
-      LOG_ERROR("graphic card does not support OpenGL 2.0");
-      graphics_context_global_uninit();
-      return 1;
-    }
+  // TODO validate if glewInit this need clean-up.
+  GLenum glew_status = glewInit();
+  if (glew_status != GLEW_OK) {
+    LOG_ERROR("glewInit %s", glewGetErrorString(glew_status));
+    graphics_context_global_uninit();
+    return 1;
+  }
+  if (!GLEW_VERSION_2_0) {
+    LOG_ERROR("graphic card does not support OpenGL 2.0");
+    graphics_context_global_uninit();
+    return 1;
   }
 
   return 0;
@@ -99,22 +98,21 @@ size_t graphics_context_global_init()
 //--------------------------------------------------------------------------------
 size_t graphics_context_global_uninit()
 {
-  {  // OpenGL related
-    if (!global_graphics_context) {
-      LOG_ERROR("global_graphics_context is not initialize")
-      return 1;
-    }
-
-    if (global_graphics_context->gl_context) {
-      SDL_GL_DeleteContext(global_graphics_context->gl_context);
-    }
-
-    if (global_graphics_context->window) {
-      SDL_GL_DeleteContext(global_graphics_context->window);
-    }
-
-    SDL_Quit();
+  // OpenGL related
+  if (!global_graphics_context) {
+    LOG_ERROR("global_graphics_context is not initialize")
+    return 1;
   }
+
+  if (global_graphics_context->gl_context) {
+    SDL_GL_DeleteContext(global_graphics_context->gl_context);
+  }
+
+  if (global_graphics_context->window) {
+    SDL_GL_DeleteContext(global_graphics_context->window);
+  }
+
+  SDL_Quit();
 
   free(global_graphics_context);
   global_graphics_context = 0x0;
@@ -134,15 +132,14 @@ size_t graphics_context_global_run(void (*main_loop_cb)(float),
     return 1;
   }
 
-  { // prepare loop callbacks.
-    global_graphics_context->main_loop_cb = main_loop_cb;
-    if (handle_hotkey_cb) {
-      global_graphics_context->handle_hotkey_cb = handle_hotkey_cb;
-    }
-
-    global_graphics_context->time_current = SDL_GetTicks();
-    global_graphics_context->time_last = global_graphics_context->time_current;
+  // prepare loop callbacks.
+  global_graphics_context->main_loop_cb = main_loop_cb;
+  if (handle_hotkey_cb) {
+    global_graphics_context->handle_hotkey_cb = handle_hotkey_cb;
   }
+
+  global_graphics_context->time_current = SDL_GetTicks();
+  global_graphics_context->time_last = global_graphics_context->time_current;
 
 
 #ifdef EMSCRIPTEN
