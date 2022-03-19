973527af851346b9cf460fe973202878a250318a
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-03-19 13:14:42 -0400

    Finished logger

--- fun/11_modern_c/src/common/log.h
+++ fun/11_modern_c/src/common/log.h
@@ -5,21 +5,24 @@
 
 #include <stdio.h>
 
-#define LOG_ERROR(...)                                     \
-  fprintf(stderr, "error : %s:%d : ", __FILE__, __LINE__); \
-  fprintf(stderr, __VA_ARGS__);                            \
-  fprintf(stderr, "\n");
+#define LOG_ERROR(...)                         \
+  print_fd(2, "[error] ");                     \
+  print_fd(2, __VA_ARGS__);                    \
+  print_fd(2, " (%s:%d)", __FILE__, __LINE__); \
+  print_fd(2, "\n");
+
 
 #ifdef DEBUG_TRACE
-#define LOG_TRACE(...)                                     \
-  fprintf(stderr, "trace : %s:%d : ", __FILE__, __LINE__); \
-  fprintf(stderr, __VA_ARGS__);                            \
-  fprintf(stderr, "\n");
+#define LOG_TRACE(...)                         \
+  print_fd(2, "[trace] ");                     \
+  print_fd(2, __VA_ARGS__);                    \
+  print_fd(2, " (%s:%d)", __FILE__, __LINE__); \
+  print_fd(2, "\n");
 #else
 #define LOG_TRACE(...) do { } while(0)
 #endif
 
-//#define PRINT(...) 
-void print_str(str str);
+void print(const char* fmt, ...);
+void print_fd(int fd, const char* fmt, ...);
 
 #endif // COMMON_LOG_H
--- fun/11_modern_c/src/test/test_common_log.c
+++ fun/11_modern_c/src/test/test_common_log.c
@@ -3,73 +3,14 @@
 #include "common/str.h"
 #include "common/log.h"
 
-#include <stdlib.h>
-#include <stdarg.h>
-
-static allocator_cbs allocator =
-{
- .malloc=&malloc,
- .realloc=&realloc,
- .free=&free
-};
-
-void print_test(const char* fmt, ...) {
- va_list vl;
- va_start(vl, fmt);
-
- char buffer [33];
- str fmt_str_itr = cstr(fmt);
- str_buf str_buf = str_buf_create(10, allocator);
-
-
- str found = str_find_first(fmt_str_itr, cstr("%")); 
-
- while(str_valid(found)) {
-  size_t found_offset = found.data - fmt_str_itr.data;
-
-  const str prefix_to_append = str_sub(fmt_str_itr, 0, found_offset);
-  str_buf_append(&str_buf, prefix_to_append);
-
-  fmt_str_itr = str_sub(fmt_str_itr, found_offset, fmt_str_itr.size);
-
-  if(fmt_str_itr.size >=2) {
-    switch(fmt_str_itr.data[1]) {
-     case 's' : {
-       const char* buf = va_arg(vl, char*);
-       str_buf_append(&str_buf, cstr(buf));
-      break;
-     }
-     case 'd' : {
-       snprintf(buffer, sizeof(buffer), "%d", va_arg(vl, int));
-       str_buf_append(&str_buf, cstr(buffer));
-      break;
-     }
-    }
-  }
-
-  fmt_str_itr = str_sub(fmt_str_itr, 2, fmt_str_itr.size);
-  found = str_find_first(fmt_str_itr, cstr("%")); 
- }
- va_end(vl);
-
- str_buf_append(&str_buf, fmt_str_itr);
- 
- print_str(str_buf_str(str_buf));
- 
- str_buf = str_buf_destroy(str_buf);
-}
 
 //--------------------
 void test_common_log_run(allocator_cbs allocator)
 {
-  
- str_buf str_buf = str_buf_create(10, allocator);
- str_buf_append(&str_buf, cstr("Hey"));
- 
- print_str(str_buf_str(str_buf));
+ print("%s:%s:%s", "test", "hey", "sup");
+ print("hey : %d",1131);
+ print_fd(2, "hey : %d",1131);
+ print_fd(2, "\n");
 
- str_buf = str_buf_destroy(str_buf);
- 
- print_test("%s:%s:%s", "test", "hey", "sup");
- print_test("hey : %d",1131);
+ LOG_ERROR("Testing the Error Logging, do not worry : %d", 1337);
 }
