d0e9fca67c86ab4ed73878996b55409df2dd1ab6
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-03-22 15:39:19 -0400

    Update dyn_array.h
    brainstorm on dyn_array

--- fun/11_modern_c/src/common/dyn_array.h
+++ fun/11_modern_c/src/common/dyn_array.h
@@ -2,17 +2,32 @@
 #define COMMON_DYN_ARRAY_H
 
 #include "common/allocator_cbs.h"
-
-typedef struct dyn_array
+/*
+typedef struct dyn_array_info
 {
-  void* data;
   size_t size;
   size_t capacity;
   size_t size_of_one;
   allocator_cbs allocator;
-} dyn_array;
+} dyn_array_info;
 
-dyn_array* dyn_array_create(size_t capacity, size_t size_of_one, allocator_cbs allocator);
+#define dyn_array_create(capacity, size_of_one, allocator, type) \
+ struct {\
+  type* data;\
+  dyn_array_info info;\  
+ }
+
+#define dyn_array_begin(dyn_array) \
+dyn_array.data
+
+#define dyn_array_end(dyn_array) \
+dyn_array.date + dyn_array.header.size
+
+
+
+ */
+
+dyn_array* _dyn_array_create(size_t capacity, size_t size_of_one, allocator_cbs allocator);
 dyn_array dyn_array_destroy(dyn_array dyn_array);
 
 void dyn_array_push_back(dyn_array dyn_array, void* elem);
