3637ce4e62233e068457e1fa4f9038e5672b33bf
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-07-01 17:06:08 -0400

    update

--- main/tetris.cpp
+++ main/tetris.cpp
@@ -1,3 +1,4 @@
+#include "glwrappers.h"
 #include "tetris.h"
 
 #include  <SDL.h>
@@ -5,9 +6,30 @@
 struct TetrisImpl {
 };
 
-Tetris::Tetris() {
+Tetris::Tetris() : self(new TetrisImpl()) {
 }
 Tetris::~Tetris() {
+}
+
+static unsigned int compile_shader(unsigned int type, const std::string& source) {
+  unsigned int id = glCreateShader(type);
+  const char* src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if(result == GL_FALSE) {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char* message = (char*) alloca(length* sizeof(char));
+    glGetShaderInfoLog(id, length, &length, message);
+    LOG_ERROR("Failed to compile shader!");
+    LOG_ERROR("%s", message);
+    glDeleteShader(id);
+    return 0;
+  }
+  return id;
 }
 
 void Tetris::Render(SDL_Window* window, bool reset, float time_delta) {
