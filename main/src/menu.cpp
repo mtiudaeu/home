d29cc080de420b4f84a9dc1a5921b70eaa3989ab
Mathieu Drapeau <m.drapeau9@gmail.com>
2022-08-14 12:57:08 -0300

    Update

--- main/src/menu.cpp
+++ main/src/menu.cpp
@@ -1,62 +1,60 @@
 #include "menu.h"
-
-#include "text.h"
 #include "square.h"
+#include "text.h"
 
 namespace menu {
 
-enum EMenu {
-  Menu,
-  Tetris,
-  End
-};
+enum EMenu { Tetris, NextGame };
 
 struct Ctx {
   EMenu e_menu;
   text::Context title;
   text::Context tetris;
+  text::Context next_game;
   square::Ctx selection;
 };
 
 //--------------------------------------------------
 Ctx* create() {
   Ctx* ctx = new Ctx();
-  ctx->e_menu = Menu;
+  //ctx->e_menu = Tetris;
+  ctx->e_menu = NextGame;
 
   ctx->title = {
-.x = -8.0f,
-.y = 6.0f,
-.size = 1.0f,
-.value = "Welcome to the menu"
-};
-  ctx->tetris = {
-.x = -8.0f,
-.y = 3.0f,
-.size = 1.0f,
-.value = "Tetris"
-};
+      .x = -8.0f, .y = 6.0f, .size = 1.0f, .value = "Welcome to the menu"};
+  ctx->tetris = {.x = -8.0f, .y = 3.0f, .size = 1.0f, .value = "Tetris"};
+  ctx->next_game = {.x = -8.0f, .y = 1.0f, .size = 1.0f, .value = "Next Game"};
 
- ctx->selection = {
-.x = -4.0f,
-.y = 1.5f,
-.height = 2.0f,
-.width = 2.0f,
-};
+  ctx->selection = {
+      .x = -4.0f,
+      .y = 1.5f,
+      .height = 2.0f,
+      .width = 2.0f,
+  };
 
   return ctx;
 }
 //--------------------------------------------------
 Ctx* destroy(Ctx* ctx) {
-  
   delete ctx;
   return nullptr;
 }
 //--------------------------------------------------
-void render(Ctx& context) {
-  square::render(context.selection);
-  text::render(context.title);
-  text::render(context.tetris);
-}
+void render(Ctx& ctx) {
+  switch (ctx.e_menu) {
+    case Tetris:
+      ctx.selection.y = ctx.tetris.y / 2.0f;
+      break;
+    case NextGame:
+      ctx.selection.y = ctx.next_game.y / 2.0f;
+      break;
+  }
 
 
+  square::render(ctx.selection);
+  text::render(ctx.title);
+  text::render(ctx.tetris);
+  text::render(ctx.next_game);
 }
+
+}  // namespace menu
